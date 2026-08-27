9e27925f55aa11c80d3baceb08b0bca2ed416387
Omaroviz <omaroviz1990@gmail.com>
2026-08-27 13:48:25 +0300

    Коммит. Создание вокалодиов и доставка их в Базу Данных

--- senbonzakura_v4/client.js
+++ senbonzakura_v4/client.js
@@ -1,12 +1,14 @@
 console.log('HELLO FROM client.JS!');
 
 const name = document.querySelector('#name');
+const code = document.querySelector('#code');
 const author = document.querySelector('#author');
 const btn = document.querySelector('#btn');
 
 btn.addEventListener('click', () => {
 	const data = {
 		text: name.value,
+		code: code.value,
 		author: author.value
 	};
 
--- senbonzakura_v4/index.js
+++ senbonzakura_v4/index.js
@@ -2,7 +2,7 @@ import http from 'node:http';
 import fs from 'node:fs';
 import db from './db.js';
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
 	if (req.method === 'GET' && req.url === '/') {
 		const html = fs.readFileSync('index.html');
 		console.log('User in /');
@@ -17,20 +17,45 @@ const server = http.createServer((req, res) => {
 	if (req.method === 'POST' && req.url === '/posts') {
 		console.log('User is want /posts');
 		let body = '';
-		const response = {
-			success: true,
-			message: 'Data is correct pulled',
-			error: false
-		};
 		req.on('data', chunk => {
 			body+=chunk;
 		});
-		req.on('end', () => {	
-			const date = JSON.parse(body);
-			console.log(date);
+		req.on('end', async () => {	
+			const data = JSON.parse(body);
+			if (!data.text.trim() || !data.author.trim() || !data.code.trim()) {
+				const response = {
+					success: false,
+					message: 'Text/Author is empty.',
+					error: true
+				};
+				res.writeHead(500, {'Content-Type': 'application/json; charset=utf-8'});
+				res.end(JSON.stringify(response));
+				return;
+			}
+			try {
+				console.log(data);
+				const [result] = await db.execute(
+					'INSERT INTO vocaloid(name, code, author) VALUES(?,?,?)',
+					[data.text.trim(), data.code.trim(), data.author.trim()]
+				);
+				const response = {
+					success: true,
+					message: 'Post is added.',
+					post: {
+						id: result.insertId,
+						title: result.title,
+						text: result.text,
+						author: result.author
+					},
+					error: false
+				};
+				res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+				res.end(JSON.stringify(response));
+
+			} catch (error) {
+				console.log('[ERROR] '+error);
+			}
 		});
-		res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-		res.end(JSON.stringify(response));
 	}
 });
 
